b7a642ece09c72ba6a31370dc87e307ea1cf97a3
SamuelPHIL <52634022+SamuelPHIL@users.noreply.github.com>
2024-01-22 20:45:05 +0300

    Created some print functions using sys and datetime modules

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
 
+import sys
+import datetime
+import os
+
+Arr1 = sys.argv[1]
+Arr2 = sys.argv[2]
+print("Right now it's "datetime.datetime.now())
+print("Let's do "Arr1 "and thereafter go to "Arr2)
