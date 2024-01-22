17676721524a3a722f5ee5c6d478a2e646179aff
Sh'mu'el <samshepherd2006@yahoo.com>
2024-01-22 21:07:20 +0300

    Corrected syntax errors in the print functions

--- rearrange.py
+++ rearrange.py
@@ -6,5 +6,5 @@ import os
 
 Arr1 = sys.argv[1]
 Arr2 = sys.argv[2]
-print("Right now it's "datetime.datetime.now())
-print("Let's do "Arr1 "and thereafter go to "Arr2)
+print("Right now it's ", datetime.datetime.now())
+print("Let's do ", Arr1, " and thereafter go to ", Arr2)
